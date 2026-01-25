f7b259238f2fa232ece78cbd46cabb2397ace889
Raunit <raunitarya599@gmail.com>
2026-01-25 22:40:23 +0530

    Add PostgreSQL configuration and Docker Compose setup

--- app/config.py
+++ app/config.py
@@ -17,6 +17,13 @@ class Settings(BaseSettings):
     AZURE_SUBSCRIPTION_ID: str = Field(..., min_length=20)
     HOST: str = Field(default="127.0.0.1")
     PORT: int = Field(default=8000)
+    POSTGRES_SERVER: str = Field(..., min_length=5)
+    POSTGRES_PORT: int = Field(..., ge=1, le=65535)
+    POSTGRES_DB: str = Field(..., min_length=3)
+    POSTGRES_USER: str = Field(..., min_length=6)
+    POSTGRES_PASSWORD: str = Field(..., min_length=4)
+    PGADMIN_EMAIL: str = Field(..., min_length=10)
+    PGADMIN_PASSWORD: str = Field(..., min_length=5)
 
 
 settings = Settings()
